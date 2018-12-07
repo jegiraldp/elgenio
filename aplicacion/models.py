d1d20427e00882f9680f1bed27df48d22061ef2c
jegiraldp <jegiraldp@gmail.com>
2018-12-07 01:32:34 -0500

    version 07122018 01:32

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class cliente(models.Model):
+    nitE = models.CharField(max_length=30, null=False, blank=False, default='0')
+    nombreE = models.CharField(max_length=50, null=False, blank=False)
+    direccionE=models.CharField(max_length=50, null=False, blank=False)
+    telefonoE=models.CharField(max_length=50,  null=False, blank=False)
+    descripcionE=models.CharField(max_length=100,  null=True, blank=False)
+
+    def __str__(self):
+        return self.nombreE + " - ("+self.telefonoE+")"
+
+
+
+
+
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import cliente
 
 def index(request):
-    ctx={'info':'-- Sistema de Información -- Extintores EL GENIO '}
+    ctx={'info':'-- Sistema de Información -- '}
     return render (request,"aplicacion/inicio.html",ctx)
+
+def getclientes(request):
+    lista = cliente.objects.order_by('nombreE')
+    ctx = {'clientes': lista}
+    return render(request, "aplicacion/listadoClientes.html", ctx)
